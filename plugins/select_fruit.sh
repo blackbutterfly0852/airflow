fbe6cf50b09ee5f640701c351372bc70647e1e24
blackbutterfly0852 <blackbutterfly0852@gmail.com>
2024-11-28 13:05:18 +0900

    241128(목) .sh

---
+++ plugins/select_fruit.sh
@@ -0,0 +1,13 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ]; then
+	echo "APPLE"
+elif [ $FRUIT == ORANGE ]; then
+	echo "ORANGE"
+elif [ $FRUIT == GRAPE ]; then
+	echo "GRAPE"
+else
+	echo "OTHERS"
+fi
+
+
+
