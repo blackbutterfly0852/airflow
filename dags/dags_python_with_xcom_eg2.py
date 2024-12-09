6b3911a336b7f21f88980c24d9c924cdd1aef254
blackbutterfly0852 <blackbutterfly0852@gmail.com>
2024-12-09 13:50:10 +0900

    241209(월) xcom

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -4,7 +4,7 @@ from airflow.decorators import task
 
 
 with DAG(
-    dag_id="dags_python_with_xcom_eg1", # 일반적으로 DagId와 파일명과 일치시키는것이 좋음
+    dag_id="dags_python_with_xcom_eg2", # 일반적으로 DagId와 파일명과 일치시키는것이 좋음
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 12, 9, tz="Asia/Seoul"),
     catchup=False # 일반적으로 false, 누락된 구간을 동시에 수행(순차적이 아님)
