7731e37d3b5b4f64b93236d828825c6b5e8de371
blackbutterfly0852 <blackbutterfly0852@gmail.com>
2024-12-11 09:06:03 +0900

    241211(수) branchPython

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -36,7 +36,7 @@ with DAG(
         item_list = ['A',' B', 'C']
         selected_item = random.choice(item_list)
         
-        if selected_item == 'A'
+        if selected_item == 'A':
             return 'task_a' # 실제 task_id 작성
         elif selected_item in ['B', 'C']:
             return ['task_b', 'task_c']
