bcd427a3c58b34f1cb4f82f6ea7f8a605448d3bf
blackbutterfly0852 <blackbutterfly0852@gmail.com>
2024-12-13 08:45:54 +0900

    241213(금) http

--- dags/dags_simple_http_operator.py
+++ dags/dags_simple_http_operator.py
@@ -18,7 +18,7 @@ with DAG(
         http_conn_id = 'openai.seoul.go.kr',
         endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/tbLnOpendataRtmsV/1/10/',
         method = 'GET',
-        header = {'Content_Type':'application-json',
+        header = {'Content-Type':'application/json',
                   'charset' : 'utf-8',
                   'Accpet' : '*/*'}
     )
