5ee0946e78b122b1550e4383b66e319859f95b35
blackbutterfly0852 <blackbutterfly0852@gmail.com>
2024-12-18 17:35:45 +0900

    241218(수) commit

--- dags/dags_external_task_sensor.py
+++ dags/dags_external_task_sensor.py
@@ -6,9 +6,9 @@ from airflow.utils.state import State
 
 with DAG(
     dag_id='dags_external_task_sensor',
-    start_date=pendulum.datetime(2024,12,18, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2024,12,15, tz='Asia/Seoul'),
     schedule='0 7 * * *',
-    catchup=False
+    catchup=True
 ) as dag:
     def calculate_execution_date(execution_date):
     # execution_date에서 6시간 전 시점 계산
