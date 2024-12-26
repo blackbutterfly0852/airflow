b390c455c44d5cae56f1389b6c0b83b02837d9c2
blackbutterfly0852 <blackbutterfly0852@gmail.com>
2024-12-26 14:38:21 +0900

    241226(목) trigger, rshiny

--- dags/dags_python_with_custom_hook_bulk_load.py
+++ dags/dags_python_with_custom_hook_bulk_load.py
@@ -6,7 +6,7 @@ from hooks.custom_postgres_hook import CustomPostGresHook
 with DAG(
     dag_id="dags_python_with_custom_hook_bulk_load", # 일반적으로 DagId와 파일명과 일치시키는것이 좋음
     schedule="0 7 * * *",
-    start_date=pendulum.datetime(2024, 12, 14, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 12, 26, tz="Asia/Seoul"),
     catchup=False # 일반적으로 false, 누락된 구간을 동시에 수행(순차적이 아님)
     
 )  as dag:
